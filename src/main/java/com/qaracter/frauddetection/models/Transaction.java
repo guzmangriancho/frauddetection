6fce5a5ec704284f8d1802d20625a2d6495ac45e
Guzmán González de Riancho <guzmangriancho@gmail.com>
2026-01-08 13:36:28 +0100

    feat: transaction model

---
+++ src/main/java/com/qaracter/frauddetection/models/Transaction.java
@@ -0,0 +1,66 @@
+package com.qaracter.frauddetection.models;
+
+public class Transaction {
+    private Long id;
+    private Long accountId;
+    private String recipient;
+    private double amount;
+    private long timestamp;
+    private boolean flagged = false;
+
+    public Transaction(Long id, Long accountId, String recipient, double amount) {
+        this.id = id;
+        this.accountId = accountId;
+        this.recipient = recipient;
+        this.amount = amount;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isFlagged() {
+        return flagged;
+    }
+
+    public void setFlagged(boolean flagged) {
+        this.flagged = flagged;
+    }
+}
